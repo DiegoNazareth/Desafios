b83e25c1739cf3ac11a58cd9320b53986297f4af
DiegoNazareth <toby72korn@hotmail.com>
2024-01-27 05:10:08 -0600

    Se agregaron los desafios resueltos

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-let numeroSecreto=6;
-let numeroUsuario=prompt("Me indicas un numero porfavor: ");
-//alert('Hola Mundo');
+//Muestra una alerta con el mensaje "¡Bienvenida y bievenido a nuestro sitio web!".
+alert('¡Bienvenida y bievenido a nuestro sitio web!');
 
-console.log(numeroUsuario);
+//Declara una variable llamada nombre y asígnale el valor "Luna".
+let nombre="Luna";
 
-if(numeroSecreto==numeroUsuario){
-    alert('Acertarste el numero');
+//Crea una variable llamada edady asígnale el valor 25.
+let edad=25;
+
+//Establece una variable numeroDeVentas y asígnale el valor 50.
+let numeroDeVentas=50;
+
+//Establece una variable saldoDisponible y asígnale el valor 1000.
+let saldoDisponible=1000.
+
+//Muestra una alerta con el texto "¡Error! Completa todos los campos".
+alert('Error! Completa todos los campos');
+
+//Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". 
+//Ahora muestra una alerta con el valor de la variable mensajeDeError .
+
+let mensajeDeError="¡Error! Completa todos los campos";
+alert(mensajeDeError);
+
+//Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre .
+let nombre1=prompt("Ingresa nombre de usuario; ");
+
+//Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+let edad1=prompt("Ingresa su edad; ");
+
+//Ahora, si la edad es mayor o igual a 18, 
+//muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
+if(edad1>=18){
+    alert("¡Puedes obtener tu licencia de conducir!");
 }
+
