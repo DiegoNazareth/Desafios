1ab4db6dd3e9b8419eecd0ba9c8fe0cbbf5f149e
DiegoNazareth <toby72korn@hotmail.com>
2024-01-27 04:14:28 -0600

    Primera actualizacion

---
+++ app.js
@@ -0,0 +1,9 @@
+let numeroSecreto=6;
+let numeroUsuario=prompt("Me indicas un numero porfavor: ");
+//alert('Hola Mundo');
+
+console.log(numeroUsuario);
+
+if(numeroSecreto==numeroUsuario){
+    alert('Acertarste el numero');
+}
